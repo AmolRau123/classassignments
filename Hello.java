c8b5bea2d5bd8d64180c49713dcdfc299d237d7d
AmolRau123 <117341114+AmolRau123@users.noreply.github.com>
2022-11-03 12:36:34 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+package basicprogram;
+import java.util.Scanner;
+public class Hello {
+public static void main(String []args)
+{
+	Scanner Scan=new Scanner(System.in);
+			long num;
+	System.out.println("enter one number");
+	num=Scan.nextInt();
+	System.out.println("The value you have entered: "+ num);
+	}
+}
