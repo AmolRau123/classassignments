7703be61c90da78b17fa302b2d104d1200eaa89d
AmolRau123 <117341114+AmolRau123@users.noreply.github.com>
2022-11-04 11:17:34 +0530

    Add files via upload

---
+++ CountNoOfDigit1.java
@@ -0,0 +1,30 @@
+package basicprogram;
+
+import java.util.Scanner;
+
+public class CountNoOfDigit1 
+{
+
+	public static void main(String[] args)
+	{
+		int n,count=0;
+		Scanner sc=new Scanner(System.in);
+		
+		//input number from user
+		System.out.println("Enter number to count:");
+		n=sc.nextInt();
+		
+		//run loop till n is greater than 0
+		while(n!=0)		//	for(;n!=0;)
+		{
+			//incrementing digit count
+			count++;
+			//remove last digit'n'
+			n=n/10;
+		}
+		//display the total digits
+		System.out.println("Total number of digits: "+ count);
+		
+}
+	}
+
